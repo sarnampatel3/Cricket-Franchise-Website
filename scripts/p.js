bfac7875a8994c55a4a587a995af8ab425aa746c
Sarnam <spate247@uncc.edu>
2022-07-05 18:41:15 -0400

    My first commit

---
+++ scripts/p.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $("li a").each(function(){
+
+        $(this).click(function(){
+
+            $.ajax({
+                type: "get",
+                url: "players/" + this.title + ".json", 
+                error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+                },
+
+                dataType: "json",
+
+                success: function(data) {
+                    $("main").empty();
+
+                    $.each(data, function(){
+
+                        $.each(this, function(key, value){
+                            $("main").append(
+                                $("<h1>" + value.name + "</h1>" + "<h2>" + value.title + "</h2>" + "<h3>" + value.bio + "</h3>")
+                            );
+                        })
+                    });
+                }
+            })
+        });
+    });
+}); 
